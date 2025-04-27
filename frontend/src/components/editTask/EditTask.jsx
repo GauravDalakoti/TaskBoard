3ceb09d20fc9284bf8562f1a82a715a4e1a7facf
gaurav dalakoti <gauravdalakoti046@gmail.com>
2025-04-28 01:41:13 +0530

    make some changes

--- frontend/src/components/editTask/EditTask.jsx
+++ frontend/src/components/editTask/EditTask.jsx
@@ -90,7 +90,7 @@ const EditTask = () => {
 
     return (
 
-        <div className='min-h-87vh'>
+        <div className='min-h-[87vh]'>
 
             {loading ?  <div role="status" className='flex justify-center text-center w-[100vw]'>
                         <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
