2ffe3e2c3da030b3d4737590f7e2b3e5638f55a0
gaurav dalakoti <gauravdalakoti046@gmail.com>
2025-04-29 22:06:32 +0530

    change text size

--- frontend/src/components/signUp/SignUp.jsx
+++ frontend/src/components/signUp/SignUp.jsx
@@ -77,7 +77,7 @@ function SignUp() {
                         <button className='bg-blue-500 hover:scale-105 w-[30vw] text-white rounded-lg px-4 py-3 font-semibold max-lg:w-[38vw] max-md:w-[60vw] max-sm:w-[80vw]' type='submit'>Sign Up</button>
                     </div>
 
-                    <div className='flex text-[0.9em] max-lg:text-[0.8em]'>
+                    <div className='flex text-[0.9em] max-lg:text-[0.6em]'>
 
                         <p className='mx-1'>I agree to the </p>
                         <a href="/" className='text-green-600 hover:text-orange-600 cursor-pointer'>terms and conditions
