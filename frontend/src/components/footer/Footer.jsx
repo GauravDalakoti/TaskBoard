4ac600fbb5c18a017254cfc205f91fb930b7b953
gaurav dalakoti <gauravdalakoti046@gmail.com>
2025-04-29 22:12:44 +0530

    change color in footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Footer() {
     return (
 
-
-        <footer className="bg-gray-700 dark:bg-gray-900">
+        <footer className="bg-gray-900 dark:bg-gray-900">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
